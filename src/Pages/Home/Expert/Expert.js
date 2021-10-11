9dbde137d8aad7dc6a1038fd0fb7b6568b125ee5
rasel <russelroy15@gmail.com>
2021-10-12 01:32:17 +0600

    WIP on responsive React website

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -3,8 +3,8 @@ import React from "react";
 const Expert = ({ expert }) => {
   const { name, img, expertize } = expert;
   return (
-    <div>
-      <img src={img} alt="photos" />
+    <div className="col-lg-4 col-md-6">
+      <img src={img} alt="photos" className="rounded" />
       <h3>{name}</h3>
       <h4>{expertize}</h4>
     </div>
