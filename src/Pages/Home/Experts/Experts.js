7560dd69a42aee4b4890e16bd4173d1465c1184e
rasel <russelroy15@gmail.com>
2021-10-12 01:22:40 +0600

    WIP on responsive React website

--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -1,4 +1,26 @@
 import React from "react";
+const experts = [
+  {
+    img: mechanic1,
+    name: "Andrew Smith",
+    expertize: "-Engine Expert-",
+  },
+  {
+    img: mechanic2,
+    name: "John Anderson",
+    expertize: "-Polish Expert-",
+  },
+  {
+    img: mechanic3,
+    name: "Zakaria Smith",
+    expertize: "-Coloring Expert-",
+  },
+  {
+    img: mechanic4,
+    name: "Sakib Anderson",
+    expertize: "-Alrounder Expert-",
+  },
+];
 
 const Experts = () => {
   return (
