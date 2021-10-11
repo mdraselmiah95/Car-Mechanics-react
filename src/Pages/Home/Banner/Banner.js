ab2ad75887495b45a72ec310aaee37bcf7dcae4c
rasel <russelroy15@gmail.com>
2021-10-12 01:50:37 +0600

    WIP on responsive React website

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,27 +1,22 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
+import banner1 from "../../../images/banner/banner1.jpg";
+import banner2 from "../../../images/banner/banner2.jpg";
+import banner3 from "../../../images/banner/banner3.jpg";
 
 const Banner = () => {
   return (
     <>
       <Carousel>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
+          <img className="d-block w-100" src={banner1} alt="First slide" />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
+          <img className="d-block w-100" src={banner2} alt="Second slide" />
 
           <Carousel.Caption>
             <h3>Second slide label</h3>
@@ -29,11 +24,7 @@ const Banner = () => {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src={banner3} alt="Third slide" />
 
           <Carousel.Caption>
             <h3>Third slide label</h3>
