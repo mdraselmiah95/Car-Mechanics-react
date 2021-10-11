20617a2042dd390094739c9ff32ff31c41fb5e9a
rasel <russelroy15@gmail.com>
2021-10-12 00:42:17 +0600

    WIP on responsive React website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Service from "./Pages/Home/Service/Service";
+import Home from "./Pages/Home/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Service></Service>
+      <Home />
     </div>
   );
 }
