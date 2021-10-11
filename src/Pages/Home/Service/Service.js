c9eab94168fcb32eec173da3aeef6156d81b8651
rasel <russelroy15@gmail.com>
2021-10-12 01:17:34 +0600

    WIP on responsive React website

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -6,7 +6,7 @@ const Service = ({ service }) => {
   return (
     <div className="service">
       <img src={img} alt="photos" />
-      <h3>{name}</h3>
+      <h3 className="my-3">{name}</h3>
       <h5>Price: {price}</h5>
       <p className="px-3">{description}</p>
     </div>
