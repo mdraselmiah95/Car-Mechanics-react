658ee9de864da681193bb64313460d215318f8fa
rasel <russelroy15@gmail.com>
2021-10-12 01:16:51 +0600

    WIP on responsive React website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./Pages/Home/Home/Home";
-
 function App() {
   return (
     <div className="App">
