19d0ec8ae8da73bbb963bdf947974ae9b52e6559
rasel <russelroy15@gmail.com>
2021-10-13 19:29:49 +0600

    WIP on car mechanics react project

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <Navbar bg="dark" variant="dark" sticky="top" className="">
+      <Navbar
+        bg="dark"
+        variant="dark"
+        sticky="top"
+        className=""
+        collapseOnSelect
+        expand="lg"
+      >
         <Container>
           <Navbar.Brand href="#home">Car Mechanics</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
-            <Nav.Link href="/home#home">Home</Nav.Link>
-            <Nav.Link href="/home#services">Services</Nav.Link>
-            <Nav.Link href="/home#experts">Experts</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/home#home">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/home#services">
+              Services
+            </Nav.Link>
+            <Nav.Link as={Link} to="/home#experts">
+              Experts
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
             <Navbar.Text>
               Signed in as: <a href="#login">Mark Otto</a>
             </Navbar.Text>
