298de864bb24a8a4b7ce33508df951e687f8f928
rasel <russelroy15@gmail.com>
2021-10-13 18:14:55 +0600

    WIP on car machanice project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home/Home";
+
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
