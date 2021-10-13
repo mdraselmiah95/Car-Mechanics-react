20d5dd2a6b483552cc73c131d14216e964379dea
rasel <russelroy15@gmail.com>
2021-10-13 18:29:12 +0600

    WIP on car mechanics react project

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import notfound from "../../images/404.png";
 
 const NotFound = () => {
   return (
     <div>
       <h2 className="text-danger fw-bold">Page not found</h2>
-      <img src={notfound} alt="notfound" className="img-fluid" />
+      <img src={notfound} alt="notfound" className="img-fluid" /> <br />
+      <Link to="/home">
+        <button className="btn btn-outline-primary">Go Back</button>
+      </Link>
     </div>
   );
 };
