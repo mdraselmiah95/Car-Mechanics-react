aab4532ba200786e26e1dd5e4e881c2d7ab86326
rasel <russelroy15@gmail.com>
2021-10-13 18:34:58 +0600

    WIP on car mechanics react project

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -9,6 +9,9 @@ const Service = ({ service }) => {
       <h3 className="my-3">{name}</h3>
       <h5>Price: {price}</h5>
       <p className="px-3">{description}</p>
+      <button className="btn btn-outline-info my-3">
+        Book {name.toLowerCase()}
+      </button>
     </div>
   );
 };
