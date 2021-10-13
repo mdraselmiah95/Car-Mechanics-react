a0ac94178be46ae951ac240a5bce8be6add347f0
rasel <russelroy15@gmail.com>
2021-10-13 19:31:57 +0600

    WIP on car mechanics react project

---
+++ src/Pages/Login/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBWbOiU4bx_0X1nEidWjSlk5_-aaEHed_8",
+  authDomain: "genius-car-mechanic-e5ae1.firebaseapp.com",
+  projectId: "genius-car-mechanic-e5ae1",
+  storageBucket: "genius-car-mechanic-e5ae1.appspot.com",
+  messagingSenderId: "259869275534",
+  appId: "1:259869275534:web:b3ae9685f9e9633c9560df",
+};
+
+export default firebaseConfig;
