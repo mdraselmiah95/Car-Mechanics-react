cfabd5bffcdff684e9bff928f96a1bd5b0f4e3f2
rasel <russelroy15@gmail.com>
2021-10-13 18:25:40 +0600

    WIP on car mechanics react project

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from "react";
+import notfound from "../../images/404.png";
 
 const NotFound = () => {
   return (
     <div>
-      <h2>This is not found.</h2>
+      <h2>Page not found</h2>
+      <img src={notfound} alt="notfound" />
     </div>
   );
 };
