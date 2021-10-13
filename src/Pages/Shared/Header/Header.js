7ecb600895f7d75d4249200b40550975cf8f3fc0
rasel <russelroy15@gmail.com>
2021-10-13 19:16:10 +0600

    WIP on car mechanics react project

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -9,9 +9,10 @@ const Header = () => {
           <Navbar.Brand href="#home">Car Mechanics</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#services">Services</Nav.Link>
-            <Nav.Link href="#experts">Experts</Nav.Link>
+            <Nav.Link href="/home#home">Home</Nav.Link>
+            <Nav.Link href="/home#services">Services</Nav.Link>
+            <Nav.Link href="/home#experts">Experts</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
             <Navbar.Text>
               Signed in as: <a href="#login">Mark Otto</a>
             </Navbar.Text>
