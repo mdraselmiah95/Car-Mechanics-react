bfd6c3b0c280c369830a1ee2e1d5480784825cb1
rasel <russelroy15@gmail.com>
2021-10-13 19:56:56 +0600

    WIP on car mechanics react project

--- src/Pages/Login/Firebase/Firebase.config.js
+++ src/Pages/Login/Firebase/Firebase.config.js
@@ -1,10 +1,11 @@
+console.log(process.env);
 const firebaseConfig = {
-  apiKey: "AIzaSyBWbOiU4bx_0X1nEidWjSlk5_-aaEHed_8",
-  authDomain: "genius-car-mechanic-e5ae1.firebaseapp.com",
-  projectId: "genius-car-mechanic-e5ae1",
-  storageBucket: "genius-car-mechanic-e5ae1.appspot.com",
-  messagingSenderId: "259869275534",
-  appId: "1:259869275534:web:b3ae9685f9e9633c9560df",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
