b1a6915384ffa83ba14d225b02c141e4122ae2ff
rasel <russelroy15@gmail.com>
2021-10-13 20:37:09 +0600

    WIP on car mechanics react project

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,10 +1,14 @@
 import React from "react";
+import useAuth from "../../../hooks/useAuth";
 
 const Login = () => {
+  const { signInUsingGoogle } = useAuth();
   return (
     <div>
       <h2>Please Login</h2>
-      <button className="btn btn-outline-info">Google Sign In</button>
+      <button onClick={signInUsingGoogle} className="btn btn-outline-info">
+        Google Sign In
+      </button>
     </div>
   );
 };
