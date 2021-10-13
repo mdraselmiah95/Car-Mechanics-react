b03e2b0040b58a2183ec8f708fde2056951996a4
rasel <russelroy15@gmail.com>
2021-10-13 19:09:38 +0600

    WIP on car mechanics react project

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -6,12 +6,16 @@ const Header = () => {
     <>
       <Navbar bg="dark" variant="dark" sticky="top" className="">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
+          <Navbar.Brand href="#home">Car Mechanics</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+            <Navbar.Text>
+              Signed in as: <a href="#login">Mark Otto</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
