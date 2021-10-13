7ebf84583de6c2fa1f1cef5390156e91033f0e5f
rasel <russelroy15@gmail.com>
2021-10-13 21:12:28 +0600

    WIP on car mechanics react project

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 import useAuth from "../../../hooks/useAuth";
 const Header = () => {
   const { user, logOut } = useAuth();
@@ -18,13 +19,13 @@ const Header = () => {
           <Navbar.Brand href="#home">Car Mechanics</Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
-            <Nav.Link as={Link} to="/home#home">
+            <Nav.Link as={HashLink} to="/home#home">
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/home#services">
+            <Nav.Link as={HashLink} to="/home#services">
               Services
             </Nav.Link>
-            <Nav.Link as={Link} to="/home#experts">
+            <Nav.Link as={HashLink} to="/home#experts">
               Experts
             </Nav.Link>
             {user.email ? (
