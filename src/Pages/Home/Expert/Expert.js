5c0c0be072e667df661e4a709e2875105995fb5e
rasel <russelroy15@gmail.com>
2021-10-25 18:27:00 +0600

    WIP on car mechanics project

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -4,9 +4,11 @@ const Expert = ({ expert }) => {
   const { name, img, expertize } = expert;
   return (
     <div className="col-lg-4 col-md-6">
-      <img src={img} alt="photos" className="rounded" />
-      <h3>{name}</h3>
-      <h4>{expertize}</h4>
+      <div className="border p-2 m-2 rounded">
+        <img src={img} alt="photos" className="rounded" />
+        <h3>{name}</h3>
+        <h4>{expertize}</h4>
+      </div>
     </div>
   );
 };
