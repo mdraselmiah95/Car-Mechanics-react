42ee07eef1517bc75a7b7de4248d9ca806e00353
rasel <russelroy15@gmail.com>
2021-10-25 19:49:01 +0600

    WIP on car mechanics project

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -3,12 +3,13 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import "./AddService.css";
 const AddService = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const onSubmit = (data) => {
     console.log(data);
     axios.post("http://localhost:5000/services", data).then((res) => {
       if (res.data.insertedId) {
         alert("Added Successfully");
+        reset();
       }
     });
   };
